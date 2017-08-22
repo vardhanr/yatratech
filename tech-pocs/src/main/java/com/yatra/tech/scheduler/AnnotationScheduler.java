47209ae68a39c10be9c35f5cd718e18b0eded8c8
vardhanr <rahul.vardhan@yatra.com>
2017-08-22 17:39:01 +0530

    schduler changes

---
+++ tech-pocs/src/main/java/com/yatra/tech/scheduler/AnnotationScheduler.java
@@ -0,0 +1,13 @@
+package com.yatra.tech.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnnotationScheduler {
+
+	@Scheduled(fixedDelay = 60 * 1000)
+	public void annotationScheduler() {
+		System.out.println("Rahul Dev Vardhan" + System.currentTimeMillis());
+	}
+}
